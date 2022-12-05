9e5cbc49af0b99a8f6102bb422e1bd7ca4916f30
Shubham Kumar (Product Architecture) <shubham.kumar@greyorange.com>
2022-12-05 07:17:03 +0000

    updates

--- create-new.sh
+++ create-new.sh
@@ -2,7 +2,7 @@
 name=$1
 year=$(date +'%Y')
 date=$(date +'%b %d, %Y')
-git checkout -b $name
+git checkout -b $name origin/draft
 mkdir -p src/pages/posts/$year/
 new_file="src/pages/posts/$year/$name.mdx"
 mkdir -p src/images/posts/$year/$name
