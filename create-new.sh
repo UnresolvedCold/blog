baf1e24ea61df6355894d088e8131674e73d4561
Shubham Kumar (Product Architecture) <shubham.kumar@greyorange.com>
2022-12-05 06:59:15 +0000

    Added a new script to create blogs

---
+++ create-new.sh
@@ -0,0 +1,22 @@
+[[ -z $1 ]] && exit 1
+name=$1
+year=$(date +'%Y')
+date=$(date +'%b %d, %Y')
+mkdir -p src/pages/posts/$year/
+new_file="src/pages/posts/$year/$name.mdx"
+mkdir -p src/images/posts/$year/$name
+
+echo "---" > $new_file
+echo "title: Some Name" >> $new_file
+echo "description: Some Description" >> $new_file
+echo "pubDate: ${date}" >> $new_file
+echo "draft: true" >> $new_file
+echo "---" >> $new_file
+echo "import BlogImage from '@components/BlogImage.astro'" >> $new_file
+echo "import image_target from '@images/posts/${year}/${name}/some-image.ext'" >> $new_file
+echo "<BlogImage src={image_target} alt='Beagle Dog by Jules D. on Unsplash' />" >> $new_file
+
+# Result
+echo "Starter file generated"
+echo "You can upload your posts images at src/images/posts/YYYY/$name"
+echo "Images can be imported using "
