ec968b17f59ad1985c1db0f0045d5974f7a262ea
Shubham Kumar <35415266+UnresolvedCold@users.noreply.github.com>
2022-12-14 00:50:40 +0530

    Monorepo migration (#2)
    * Problem description
    
    * blob, tree, commit
    
    * directories inside .git
    
    * filter-branch
    
    * Completed
    
    * Ready to be published
    
    Co-authored-by: Shubham Kumar (Product Architecture) <shubham.kumar@greyorange.com>

--- create-new.sh
+++ create-new.sh
@@ -3,7 +3,7 @@ name=$1
 year=$(date +'%Y')
 date=$(date +'%b %d, %Y')
 git fetch origin
-git checkout -b $name origin/draft
+git checkout -b $name origin/publish
 mkdir -p src/pages/posts/$year/
 new_file="src/pages/posts/$year/$name.mdx"
 mkdir -p src/images/posts/$year/$name
@@ -13,10 +13,8 @@ echo "title: Some Name" >> $new_file
 echo "description: Some Description" >> $new_file
 echo "pubDate: ${date}" >> $new_file
 echo "draft: true" >> $new_file
-echo "---" >> $new_file
 echo "import BlogImage from '@components/BlogImage.astro'" >> $new_file
-echo "import image_target from '@images/posts/${year}/${name}/some-image.ext'" >> $new_file
-echo "<BlogImage src={image_target} alt='Beagle Dog by Jules D. on Unsplash' />" >> $new_file
+echo "---" >> $new_file
 
 # Result
 echo "Starter file generated"
