71bc331ac9c66a55321ae75e6faca199f5ed865a
Shubham Kumar (Product Architecture) <shubham.kumar@greyorange.com>
2022-12-16 20:07:11 +0000

    Utterences comment added but it's not changing the theme dynamically

--- astro.config.ts
+++ astro.config.ts
@@ -6,43 +6,35 @@ import mdx from "@astrojs/mdx";
 import image from "@astrojs/image";
 import astroLayouts from "astro-layouts";
 import { remarkReadingTime } from './src/library/remark/remark-reading-time.mjs';
+import svelte from '@astrojs/svelte';
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://shubham.codes",
   markdown: {
     shikiConfig: {
-      theme: "dracula",
+      theme: "dracula"
     },
     remarkPlugins: [remarkReadingTime],
-    extendDefaultPlugins: true,
+    extendDefaultPlugins: true
   },
-  
+  output: 'server',
   base: "/",
-  integrations: [
-    compress({
-      css: true,
-      html: true,
-      js: true,
-      img: true,
-      svg: true,
-      logger: 0,
-    }),
-    tailwind(),
-    sitemap(),
-    mdx({
-      remarkPlugins: [
-        [
-          astroLayouts,
-          {
-            default: "@layouts/Layout.astro",
-            posts: "@layouts/BlogLayout.astro",
-          },
-        ],
-      ],
-    }),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
-  ],
+  integrations: [compress({
+    css: true,
+    html: true,
+    js: true,
+    img: true,
+    svg: true,
+    logger: 0
+  }), tailwind(), sitemap(), mdx({
+    remarkPlugins: [[astroLayouts, {
+      default: "@layouts/Layout.astro",
+      posts: "@layouts/BlogLayout.astro"
+    }]]
+  }), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), svelte()]
 });
