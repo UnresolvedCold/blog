4d835e73817f9df031484fc5ad003433ba48f580
Shubham Kumar (Product Architecture) <shubham.kumar@greyorange.com>
2023-01-12 08:13:35 +0000

    Default to ideaDate on creation

--- create-new.sh
+++ create-new.sh
@@ -10,13 +10,13 @@ mkdir -p src/pages/posts/$year/
 new_file="src/pages/posts/$year/$name.mdx"
 mkdir -p src/images/posts/$year/$name
 
+echo "import BlogImage from '@components/BlogImage.astro'" >> $new_file
 echo "---" > $new_file
 echo "title: Some Name" >> $new_file
 echo "description: Some Description" >> $new_file
-echo "pubDate: ${date}" >> $new_file
 echo "draft: true" >> $new_file
+echo "ideaDate: $date" >> $new_file
 echo "---" >> $new_file
-echo "import BlogImage from '@components/BlogImage.astro'" >> $new_file
 
 # Result
 echo "Starter file generated"
